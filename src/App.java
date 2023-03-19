e81a7e331304f6fd2e5b0b2a0fd685bd96ddf96b
sergiy <sergiy.ivasyuk200@gmail.com>
2023-03-19 17:12:08 +0200

    Tasks completed

--- src/App.java
+++ src/App.java
@@ -13,19 +13,33 @@ public class App {
     }
     public static class Sergiy{
         public boolean prost(int n){
-            return true;
+            for(int i=1;i<=n;i++){
+                if(i==1 || i==n)
+                    continue;
+                if(n%i==0)
+                    return false;
+            }
+                return true;
+            
         }
         public int max(int[] arr){
-            return 1;
+
+            int max=Integer.MIN_VALUE
+                    for(int i=0;i<arr.length;i++){
+                        if(arr[i]>max)
+                            max=arr[i];
+                    }
+                    return max;
+
         }
 
     }
     public static class Viktor{
-        public int sumpar(){
+        public int sumpar(int [] arr){
             return 1;
         }
-        public char[] thesar(char[]){
-
+        public char[] thesar(char[] arr){
+            return arr;
         }
     }
 }
