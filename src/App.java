750f7cca552b9d3078b4ab998814d94d7a35fe95
ViktorPylypii <viktorpuluiy@gmail.com>
2023-03-19 17:22:20 +0200

    change Class Viktor

--- src/App.java
+++ src/App.java
@@ -21,10 +21,23 @@ public class App {
 
     }
     public static class Viktor{
-        public int sumpar(){
-            return 1;
+        public int sumpar(int [] arr){
+            sum=0;
+            for(int i=0; i<arr.length;i++){
+                if(arr[i]%2==0){
+                    sum=sum+arr[i];
+                }
+            }
+            return sum;
         }
-        public char[] thesar(char[]){
+        public char[] thesar(char[] arr){
+            size=arr.length;
+            char arr2 = new char [size];
+            for(int i = 0; i<arr.length;i++){
+                arr2[i]=arr[i]+3;
+            }
+            return arr2;
+        }
 
         }
     }
