e36af64607aad19f0de35d26248f0f93b8ae21b3
p3ngi1n <igorkorolj@gmail.com>
2023-03-19 17:21:22 +0200

    rishiv sadazku

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        igor test = new igor();
+        int[] arr = {1,2,2,1};
+        int[] dar = {3,2,1,1};
+        int[] arr1 = {1,2,3,4,5,6,7,8,9};
     }
     public static class igor{
         public boolean polimodr(int[] arr){
+            int i=0;
+            int max =arr.length-1;
+            while(i<arr.length){
+                if(i!=max){
+                    return false;
+                }
+                i++;
+                max--;
+            }
             return true;
         }
         public int[] nepar(int[] arr){
@@ -21,11 +33,11 @@ public class App {
 
     }
     public static class Viktor{
-        public int sumpar(){
+        public int sumpar(int [] arr){
             return 1;
         }
-        public char[] thesar(char[]){
-
+        public char[] thesar(char[] arr){
+            return arr;
         }
     }
 }
