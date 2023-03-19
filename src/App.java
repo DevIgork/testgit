4895228731aeb431543bf1103a3d8b52674458c6
p3ngi1n <igorkorolj@gmail.com>
2023-03-19 16:46:15 +0200

    update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    }
+    public static class igor{
+        public boolean polimodr(int[] arr){
+            return true;
+        }
+        public int[] nepar(int[] arr){
+            return arr;
+
+        }
+    }
+    public static class Sergiy{
+        public boolean prost(int n){
+            return true;
+        }
+        public int max(int[] arr){
+            return 1;
+        }
+
+    }
+    public static class Viktor{
+        public int sumpar(){
+            return 1;
+        }
+        public char[] thesar(char[]){
+
+        }
     }
 }
+
